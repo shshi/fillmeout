4abc5a057ce58eed2d08f1d294bb7bd17b0fc00d
Shaohua <cell.fantasy@qq.com>
2019-04-01 17:12:08 +0700

    Update ques.py

--- ques.py
+++ ques.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 #DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:////ques.db')
-DATABASE_URL = os.environ.get('sqlite:////ques.db')
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 db = SQLAlchemy(app)
