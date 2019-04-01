c7d69f981103dab9657639eb1c852220f906fc1b
Shaohua <cell.fantasy@qq.com>
2019-04-01 17:16:09 +0700

    Update ques.py

--- ques.py
+++ ques.py
@@ -44,6 +44,6 @@ def user():
 
 if __name__ == '__main__':
 	db.create_all()
-	#app.run(debug=True)
-	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=port, debug=True)	
+	app.run(debug=True)
+	#port = int(os.environ.get('PORT', 5000))
+	#app.run(host='0.0.0.0', port=port, debug=True)	
