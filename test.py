048e1bb7408ca53364787edbb9dabdcae05c3245
Shaohua <cell.fantasy@qq.com>
2019-04-01 18:55:09 +0700

    Update test.py

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:////ques.db')
-#DATABASE_URL = os.environ.get('DATABASE_URL')
+#DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:////ques.db')
+DATABASE_URL = os.environ.get('sqlite:////ques.db')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 db = SQLAlchemy(app)
@@ -32,8 +32,15 @@ class User(db.Model):
 
 @app.route("/", methods=['GET'])
 def mainpage():
-	return "hello world"
+	return render_template('ques.html', data=User.query.all())
 
+@app.route('/data', methods=['POST'])
+def user():
+	print ("insert now")
+	u = User(request.form['stu_id'], request.form['name'], request.form['passport'], request.form['nationality'], request.form['grade'], request.form['major'], request.form['remarks'])
+	db.session.add(u)
+	db.session.commit()
+	return redirect(url_for('mainpage'))
 
 if __name__ == '__main__':
 	db.create_all()
