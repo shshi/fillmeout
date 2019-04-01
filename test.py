5803ed484cd772e45fecca645b696cdbe9e2c4e5
Shaohua <cell.fantasy@qq.com>
2019-04-01 19:07:49 +0700

    Update test.py

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 #DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:////ques.db')
 #DATABASE_URL = os.environ.get('sqlite:////ques.db')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////ques.db'#DATABASE_URL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://saayjgqzumrtix:5997bdca5394f21351a8f622d4833ec9f01751d3f1c39674dcb38029e4807a39@ec2-184-73-153-64.compute-1.amazonaws.com:5432/dblhph3s6fmvtu'#DATABASE_URL
 db = SQLAlchemy(app)
 
 class User(db.Model):
