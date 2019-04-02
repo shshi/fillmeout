77021a5c2c733da6819783aa5481baeadd0b899a
Shaohua <cell.fantasy@qq.com>
2019-04-02 16:13:59 +0700

    Update test.py

--- test.py
+++ test.py
@@ -5,10 +5,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:////ques.db')
-#DATABASE_URL = os.environ.get('sqlite:////ques.db')
+#DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://postgres:2643383@localhost/proxyget')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://saayjgqzumrtix:5997bdca5394f21351a8f622d4833ec9f01751d3f1c39674dcb38029e4807a39@ec2-184-73-153-64.compute-1.amazonaws.com:5432/dblhph3s6fmvtu'#DATABASE_URL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:2643383@localhost/proxyget'#DATABASE_URL
 db = SQLAlchemy(app)
 
 class User(db.Model):
@@ -32,7 +31,7 @@ class User(db.Model):
 
 @app.route("/", methods=['GET'])
 def mainpage():
-	return render_template('ques.html', data=User.query.all())
+	return render_template('q.html', data=User.query.all())
 
 @app.route('/data', methods=['POST'])
 def user():
@@ -45,5 +44,3 @@ def user():
 if __name__ == '__main__':
 	db.create_all()
 	app.run(debug=True)
-	#port = int(os.environ.get('PORT', 5000))
-	#app.run(host='0.0.0.0', port=port, debug=True)	
